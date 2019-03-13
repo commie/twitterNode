f752f196378cb914f89c4e5e34fcebb1b2acc08c
Alexander Savelyev <savelyev@txstate.edu>
2019-03-13 12:12:07 -0500

    Options template with space for keywords

---
+++ crawlerOptionsTemplate.js
@@ -0,0 +1,13 @@
+exports.options = {
+
+	bbox: 			"",						// bounding box as "sw_lon,sw_lat,ne_lon,ne_lat"
+	keyw: 			"",						// comma-separated keywords to track; use spaces as "AND"
+	name: 			"prefixForDataFiles", 
+
+	// Credentials for this account
+	consumerKey: 	"",
+	consumerSecret: "",
+	accessToken: 	"",
+	accessSecret: 	""
+	
+};
